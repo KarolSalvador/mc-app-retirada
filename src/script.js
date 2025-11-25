c7e6e4ff25019d69bc1d6e8f481546b40d692a39
Iagolara <iagodekara@gmail.com>
2025-11-25 17:33:20 -0300

    feat(front): Add geolocation functionality and improve button navigation

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 function selecionarOpcao(opcao) {
     if (opcao === 'retirar') {
-        window.location.href = "restaurante.html"; 
+        window.location.href = "restaurante.html";
         
     } else if (opcao === 'delivery') {
-        // 🛑 Opção Delivery: Não faz nada (apenas mostra um aviso no console)
+        
         console.log("Funcionalidade de Delivery ainda não implementada.");
-        // alert("Entrega por delivery indisponível no momento."); 
+        
         
     } else {
         console.error("Opção de pedido inválida: " + opcao);
